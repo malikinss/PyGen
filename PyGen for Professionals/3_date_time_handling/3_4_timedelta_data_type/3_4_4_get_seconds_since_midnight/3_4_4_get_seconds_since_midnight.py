1964b219f21bb2efa39cd539bba5328534ef26e2
Sam Malikin <workmalikin@gmail.com>
2025-02-14 23:02:21 +0200

    Add program to calculate seconds since midnight ⏰
    - Implemented a program that calculates the number of seconds that have passed since midnight based on a given time input.
    - Converts the input time (in "HH:MM:SS" format) into total seconds.
    - Useful for time-based calculations and analysis.

--- PyGen for Professionals/3_date_time_handling/3_4_timedelta_data_type/3_4_4_get_seconds_since_midnight/3_4_4_get_seconds_since_midnight.py
+++ PyGen for Professionals/3_date_time_handling/3_4_timedelta_data_type/3_4_4_get_seconds_since_midnight/3_4_4_get_seconds_since_midnight.py
@@ -1,32 +1,39 @@
 '''
-TODO:   
-        Write a program that takes time as input and outputs the integer number of seconds that have passed since the beginning of the day.
+TODO:
+    Write a program that takes time as input and outputs the integer number of
+    seconds that have passed since the beginning of the day.
 
-NOTE:   
-        The beginning of the day is considered to be the moment in time corresponding to 00:00:00    
+NOTE:
+    The beginning of the day is considered to be the moment in time
+    corresponding to 00:00:00
 '''
-
 from datetime import datetime
 
-TIME_FORMAT = '%H:%M:%S'
+TIME_FORMAT = "%H:%M:%S"
 
-def get_user_time():
-    """Prompt the user to input time and return as a datetime object."""
-    user_input = input('Please input the time in the format HH:MM:SS: ')
 
-    return datetime.strptime(user_input, TIME_FORMAT)
+def get_seconds_since_midnight(time_str: str) -> int:
+    """
+    Calculates the number of seconds that have passed since the beginning\
+    of the day.
 
-def get_time_since_midnight(current_time):
-    """Calculate the time difference between the given time and midnight."""
-    time_since_midnight = current_time - current_time.replace(hour=0, minute=0, second=0)
+    Args:
+        time_str (str): A time string in the format "HH:MM:SS".
 
-    return time_since_midnight
+    Returns:
+        int: The number of seconds since midnight (00:00:00).
+    """
+    current_time = datetime.strptime(time_str, TIME_FORMAT)
+    secs_in_hours = current_time.hour * 3600
+    secs_in_minutes = current_time.minute * 60
 
-def print_seconds_since_midnight(giving_time):
-    """Print the integer number of seconds that have passed since the beginning of the day."""
-    time_since_midnight = get_time_since_midnight(giving_time)
-    seconds_since_midnight = int(time_since_midnight.total_seconds())
-    print(seconds_since_midnight)
+    seconds_since_midnight = (
+        secs_in_hours + secs_in_minutes + current_time.second
+    )
 
-giving_time = get_user_time()
-print_seconds_since_midnight(giving_time)
+    return seconds_since_midnight
+
+
+# Example usage
+user_time = input("Please input the time in the format HH:MM:SS: ")
+print(get_seconds_since_midnight(user_time))
--- PyGen for Professionals/3_date_time_handling/3_4_timedelta_data_type/3_4_4_get_seconds_since_midnight/readme.md
+++ PyGen for Professionals/3_date_time_handling/3_4_timedelta_data_type/3_4_4_get_seconds_since_midnight/readme.md
@@ -1,13 +1,37 @@
-Lesson 3.4: timedelta data type
+# Get Seconds Since Midnight ⏰
 
-This lesson has 10 practical tasks and 11 theoretical questions preseneted on the website
-https://stepik.org/lesson/570050/step/1?unit=564593
+## Description 📝
 
-Lesson Topic: Timedelta Data Type
-Timedelta Data Type
-Days, Seconds, Microseconds Attributes
-Total_seconds() Method
-Comparison of Time Intervals
-Operations on Timedelta Time Intervals
-Operations on Datetime and Date
-Abstract. The lesson is devoted to the timedelta data type from the datetime module.
+This program takes a time as input and calculates the number of seconds that have passed since midnight (00:00:00).
+
+## Purpose 🎯
+
+The goal of this program is to convert a given time (in "HH:MM:SS" format) into the total number of seconds that have passed since the beginning of the day.
+
+## How It Works 🔍
+
+1. The user provides a time string in the format "HH:MM:SS".
+2. The program parses the input string into a `datetime` object.
+3. It then calculates the total number of seconds by multiplying the number of hours by 3600 (seconds in an hour), minutes by 60, and adding the number of seconds.
+4. The program then outputs the total number of seconds since midnight.
+
+### Example:
+
+```python
+Please input the time in the format HH:MM:SS: 12:30:45
+45045
+```
+
+## Output 📜
+
+The program outputs a single integer representing the number of seconds since midnight (00:00:00).
+
+## Usage 📦
+
+1. Run the program.
+2. Enter a time in the format "HH:MM:SS".
+3. The program will display the number of seconds that have passed since midnight for the entered time.
+
+## Conclusion 🚀
+
+This program is useful for converting time into a measurable unit (seconds), allowing for easy calculations or time-related analysis.
