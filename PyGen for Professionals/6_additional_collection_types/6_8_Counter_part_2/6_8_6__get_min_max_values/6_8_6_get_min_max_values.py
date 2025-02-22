123185af1a36d8dde6f1e489b5d9bca649f407d6
Sam Malikin <workmalikin@gmail.com>
2025-02-22 21:41:35 +0200

    init 6_8_6

---
+++ PyGen for Professionals/6_additional_collection_types/6_8_Counter_part_2/6_8_6__get_min_max_values/6_8_6_get_min_max_values.py
@@ -0,0 +1,75 @@
+'''
+TODO:
+        You have access to the variable data, which contains the Counter
+        dictionary.
+
+        Complete the code below to add two attributes to this dictionary:
+            - min_values() is a function that returns a list of elements with
+            the smallest value
+            - max_values() is a function that returns a list of elements with
+            the largest value
+
+        Both functions must not take any arguments.
+
+NOTE:
+        We will consider a tuple (key, value) as a dictionary element.
+
+        The dictionary elements in the lists returned by the functions must be
+        in their original order.
+
+        The functions data.min_values() and data.max_values() must take into
+        account the contents of the dictionary.
+
+        For example, if a new key-value pair is added to the data dictionary,
+        then the key and value must also be present in the lists returned by
+        the functions.
+
+        The program must not output anything.
+'''
+from collections import Counter
+from typing import Dict, List, Tuple
+
+
+def filter_items_by_specific_value(given_dict: Dict[str, int],
+                                   given_value: int) -> List[Tuple[str, int]]:
+    """
+    Filters a dictionary to return keys that have a specific value.
+
+    Args:
+        given_dict (Dict[str, int]): The dictionary to filter.
+        given_value (int): The value to filter by.
+
+    Returns:
+        List[Tuple[str, int]]: A list of (key, value) tuples with the
+        specified value.
+    """
+    return [item for item in given_dict.items() if item[1] == given_value]
+
+
+def get_max_values(self: Counter) -> List[Tuple[str, int]]:
+    """
+    Returns a list of elements with the largest value in the Counter.
+
+    Returns:
+        List[Tuple[str, int]]: A list of (key, value) tuples with the largest
+        value.
+    """
+    return filter_items_by_specific_value(self, max(self.values()))
+
+
+def get_min_values(self: Counter) -> List[Tuple[str, int]]:
+    """
+    Returns a list of elements with the smallest value in the Counter.
+
+    Returns:
+        List[Tuple[str, int]]: A list of (key, value) tuples with the smallest
+        value.
+    """
+    return filter_items_by_specific_value(self, min(self.values()))
+
+
+data = Counter('aksjaskfjsklfjdslkfjajfopewtoieqpwdpqworiiqjskanvmcxbmpewrqopkqwlmdzczmxvmvlnjpjqpkqzxvmbowiqeorewi')
+
+# Add the methods to the Counter class
+Counter.max_values = get_max_values
+Counter.min_values = get_min_values
