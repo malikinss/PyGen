6a418486f2e2e0cf20a46987804d8951db377dfa
Sam Malikin <workmalikin@gmail.com>
2025-02-23 22:14:59 +0200

    Create 6_10_1_get_all_values.py

---
+++ PyGen for Professionals/6_additional_collection_types/6_10_ChainMap_part_2/6_10_1_get_all_values/6_10_1_get_all_values.py
@@ -0,0 +1,33 @@
+'''
+TODO:
+        Implement the get_all_values() function that takes two arguments in
+        the following order:
+            - chainmap is a ChainMap whose elements are dictionaries
+            - key is an arbitrary object
+
+        The function must return a set whose elements are all the values for
+        the key from all the dictionaries in the chainmap. If the key is not
+        in the chainmap, the function must return an empty set.
+
+NOTE:
+        The ChainMap passed to the function is guaranteed to contain
+        dictionaries with hashable values.
+'''
+from collections import ChainMap
+from typing import Any, Set
+
+
+def get_all_values(chainmap: ChainMap, key: Any) -> Set[Any]:
+    """
+    Returns a set of all values associated with the given key from all
+    dictionaries in the ChainMap.
+
+    Args:
+        chainmap (ChainMap): A ChainMap containing multiple dictionaries.
+        key (Any): The key to search for in the dictionaries.
+
+    Returns:
+        Set[Any]: A set of values corresponding to the key in the ChainMap.
+                  Returns an empty set if the key is not found.
+    """
+    return {element[key] for element in chainmap.maps if key in element}
