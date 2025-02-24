83ccf30edd6d04545640c1d9388a5b732c1c5580
Sam Malikin <workmalikin@gmail.com>
2025-02-24 18:45:48 +0200

    init 7_1_4

---
+++ PyGen for Professionals/7_exception_handling/7_1_exception_handling_part_1/7_1_4_get_max_index/7_1_4_get_max_index.py
@@ -0,0 +1,30 @@
+'''
+TODO:
+    It was required to implement a function get_max_index() that takes a list
+    of distinct integers as an argument and returns the index of the largest
+    number from this list (starting with 0).
+
+    The programmer was in a hurry and implemented the function incorrectly.
+
+    Find and fix all the errors made in the implementation of this function
+    (there are exactly 4 of them).
+NOTE:
+    It is known that each error affects only one string and can be corrected
+    without changing other strings.
+'''
+# original code
+def get_max_index(numbers):
+    max_index = 0
+    max_value = numbers[-1] 
+
+    for index, value in enumerate(numbers, 1):
+        if index > max_index:
+            max_index = index
+            max_value = value
+
+    return max_value
+
+
+# fixed code
+def get_max_index(numbers):
+    return numbers.index(max(numbers))
