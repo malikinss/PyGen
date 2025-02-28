7d739501f494d282e9f339922b1d70456c04f1d9
Sam Malikin <workmalikin@gmail.com>
2025-03-01 01:36:05 +0200

    init 8_2_4

---
+++ PyGen for Professionals/8_recursion/8_2_recursion_part_2/8_2_4_reverse_sequence/8_2_4_reverse_sequence.py
@@ -0,0 +1,45 @@
+'''
+TODO:
+    Given a sequence of integers, write a program using recursion that prints
+    the sequence in reverse order.
+
+    The program is fed a sequence of integers, each on a separate line.
+
+    The sequence ends with the number 0.
+'''
+
+
+def reverse_sequence() -> None:
+    """
+    Recursively prints a sequence of integers in reverse order. The sequence
+    ends with the number 0, which is also printed but not considered part of
+    the sequence.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    def _print_element_recursive() -> None:
+        """
+        Reads an integer input from the user and prints the entire sequence in
+        reverse order once the number 0 is encountered.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        number = int(input())
+
+        if number != 0:
+            _print_element_recursive()
+
+        print(number)
+
+    _print_element_recursive()
+
+
+reverse_sequence()
