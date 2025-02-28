19cf64a1bd1e5a66616f91421e441d666d1e5638
Sam Malikin <workmalikin@gmail.com>
2025-03-01 00:04:26 +0200

    init 7_5_3

---
+++ PyGen for Professionals/7_exception_handling/7_5_exception_handling_part_5/7_5_3_prompt_for_password/7_5_3_prompt_for_password.py
@@ -0,0 +1,149 @@
+'''
+TODO:
+        Let's call a password good if:
+            - its length is 9 or more characters
+            - it contains upper and lower case letters of any alphabet
+            - it contains at least one digit
+
+        Write a program that requires entering a new password until a good one
+        is entered.
+
+        Input data format:
+            The program is given an arbitrary number of passwords, each on a
+            separate line. It is guaranteed that there is a good one
+            among them.
+
+        Output data format:
+            For each password entered, the program should output the text:
+                - LengthError, if the length of the entered password is less
+                than 9 characters
+                - LetterError, if all the letters in it have the same case or
+                are missing
+                - DigitError, if there are no digits in it
+                - Success!, if the entered password is good
+
+        After entering a good password, all subsequent passwords should
+        be ignored.
+
+NOTE:
+        The priority of the error message output in case of failure of several
+        conditions is LengthError, then LetterError, and then DigitError.
+'''
+
+
+class PasswordError(Exception):
+    pass
+
+
+class LengthError(Exception):
+    pass
+
+
+class LetterError(Exception):
+    pass
+
+
+class DigitError(Exception):
+    pass
+
+
+def is_sufficient_length(password: str) -> bool:
+    """
+    Checks if the password length is 9 characters or more.
+
+    Arguments:
+        password (str): The password to check.
+
+    Returns:
+        bool: True if the password length is >= 9, otherwise False.
+    """
+    return len(password) >= 9
+
+
+def has_uppercase(password: str) -> bool:
+    """
+    Checks if the password contains at least one uppercase letter.
+
+    Arguments:
+        password (str): The password to check.
+
+    Returns:
+        bool: True if there is at least one uppercase letter, otherwise False.
+    """
+    return any(char.isupper() for char in password)
+
+
+def has_lowercase(password: str) -> bool:
+    """
+    Checks if the password contains at least one lowercase letter.
+
+    Arguments:
+        password (str): The password to check.
+
+    Returns:
+        bool: True if there is at least one lowercase letter, otherwise False.
+    """
+    return any(char.islower() for char in password)
+
+
+def has_digit(password: str) -> bool:
+    """
+    Checks if the password contains at least one digit.
+
+    Arguments:
+        password (str): The password to check.
+
+    Returns:
+        bool: True if there is at least one digit, otherwise False.
+    """
+    return any(char.isdigit() for char in password)
+
+
+def validate_password(password: str) -> bool:
+    """
+    Checks if the password is good using the LBYL (Look Before You Leap)
+    approach.
+
+    A good password meets the following criteria:
+    - At least 9 characters long
+    - Contains both uppercase and lowercase letters
+    - Contains at least one digit
+
+    Arguments:
+        password (str): The password to check.
+
+    Returns:
+        bool: True if the password meets all criteria, otherwise False.
+    """
+    if not is_sufficient_length(password):
+        raise LengthError
+
+    if not has_uppercase(password) or not has_lowercase(password):
+        raise LetterError
+
+    if not has_digit(password):
+        raise DigitError
+
+    return True
+
+
+def prompt_for_password():
+    """
+    Continuously prompts the user for a password until a valid one is entered.
+
+    Raises:
+        LengthError, LetterError, DigitError: If the password does not meet
+        criteria.
+    """
+    while True:
+        try:
+            password = input()
+            validate_password(password)
+            break
+        except PasswordError as e:
+            print(type(e).__name__)
+
+    print('Success!')
+
+
+prompt_for_password()
