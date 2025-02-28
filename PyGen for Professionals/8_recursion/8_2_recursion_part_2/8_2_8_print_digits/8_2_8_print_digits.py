a364765a2cede697aaee2bef66fc23b013987a33
Sam Malikin <workmalikin@gmail.com>
2025-03-01 01:44:06 +0200

    init 8_2_8

---
+++ PyGen for Professionals/8_recursion/8_2_recursion_part_2/8_2_8_print_digits/8_2_8_print_digits.py
@@ -0,0 +1,28 @@
+'''
+TODO:
+    Implement a print_digits() function using recursion that takes
+    one argument:
+        number — a natural number
+
+    The function should print all the digits of number, starting with the
+    least significant digits, each on a separate line.
+'''
+
+
+def print_digits(number: int) -> None:
+    """
+    Prints all the digits of the given number, starting with the least
+    significant digit, each on a separate line.
+
+    Args:
+        number (int): A natural number.
+
+    Returns:
+        None: This function does not return a value.
+    """
+    if number > 0:
+        print(number % 10)
+        print_digits(number // 10)
+
+
+print_digits(12345)
