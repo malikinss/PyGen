1e7dcc032694ee3ca727581bded996c9dc3efc37
Sam Malikin <workmalikin@gmail.com>
2025-03-01 01:36:58 +0200

    init 8_2_7

---
+++ PyGen for Professionals/8_recursion/8_2_recursion_part_2/8_2_7_draw_hourglass/8_2_7_draw_hourglass.py
@@ -0,0 +1,52 @@
+'''
+TODO:
+    Write a program using a recursive function that prints an hourglass image
+    made up of the digits 1, 2, 3, and 4:
+        1111111111111111    # 16 times
+         2222222222222      # 12 times
+            33333333        # 8 times
+              4444          # 4 times
+            33333333        # 8 times
+         2222222222222      # 12 times
+        1111111111111111    # 16 times
+'''
+
+
+def display_layer(layer_str: str) -> None:
+    """
+    Displays a single layer of the hourglass.
+
+    Args:
+        layer_str (str): The string of digits to be displayed.
+    """
+    print(layer_str.center(16))
+
+
+def draw_hourglass() -> None:
+    """
+    Draws an hourglass shape using digits 1 through 4, recursively.
+
+    The hourglass is symmetrical with decreasing and increasing layers
+    from top to bottom.
+    """
+
+    def draw_layer(digit_value: int, repeat_count: int) -> None:
+        """
+        Recursively draws each layer of the hourglass.
+
+        Args:
+            digit_value (int): The digit to be repeated in the layer.
+            repeat_count (int): The number of times the digit is repeated.
+        """
+        layer_str = str(digit_value) * repeat_count
+
+        if repeat_count > 4:
+            display_layer(layer_str)  # Display the current layer
+            draw_layer(digit_value + 1, repeat_count - 4)
+
+        display_layer(layer_str)  # Display the mirrored layer
+
+    draw_layer(1, 16)
+
+
+draw_hourglass()
