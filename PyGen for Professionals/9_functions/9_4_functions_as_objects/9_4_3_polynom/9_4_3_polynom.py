726940b5849b944c0408e86feeb79b1b39234a98
Sam Malikin <workmalikin@gmail.com>
2025-03-01 23:10:00 +0200

    init 9_4_3

---
+++ PyGen for Professionals/9_functions/9_4_functions_as_objects/9_4_3_polynom/9_4_3_polynom.py
@@ -0,0 +1,27 @@
+'''
+TODO:
+        Implement a function polynom() that takes one argument:
+            x is a real number
+
+        The function should return the value of the expression x ^2+1.
+
+        The function should also have a values attribute that is a set of all
+        the values of the function that have already been calculated.
+'''
+
+
+def polynom(x: float) -> float:
+    """
+    Computes the polynomial expression x^2 + 1 and stores the result.
+
+    Args:
+        x (float): The input real number.
+
+    Returns:
+        float: The result of the polynomial expression.
+    """
+    polynom.__dict__.setdefault('values', set())
+    computed_value = x**2 + 1
+    polynom.values.add(computed_value)
+
+    return computed_value
