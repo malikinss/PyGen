d1d5b51780526121cab63e7e5eb1fdee1be01d92
Sam Malikin <workmalikin@gmail.com>
2025-03-01 23:09:36 +0200

    init 9_4_1

---
+++ PyGen for Professionals/9_functions/9_4_functions_as_objects/9_4_1_numbers_sum/9_4_1_numbers_sum.py
@@ -0,0 +1,36 @@
+'''
+TODO:
+        Implement a numbers_sum() function that takes one argument:
+            elems — a list of arbitrary objects
+
+        The function should return the sum of the numbers (int and float types)
+        in the elems list, ignoring any non-numeric objects.
+
+        If there are no numbers in the elems list, the function should return
+        the number 0.
+
+        The function should also have the following docstring:
+            Take a list and return the sum of its numbers (int, float),
+            ignoring non-numeric objects.
+            0 - if there are no numbers in the list.
+'''
+from typing import List, Any
+
+
+def numbers_sum(elems: List[Any]) -> float:
+    """
+    Take a list and return the sum of its numbers (int, float),
+    ignoring non-numeric objects.
+    0 - if there are no numbers in the list.
+
+    Args:
+        elems (List[Any]): The list of arbitrary objects.
+
+    Returns:
+        float: The sum of numeric elements in the list or 0 if no numbers
+        are found.
+    """
+    def _is_number(obj: Any) -> bool:
+        return isinstance(obj, (int, float))
+
+    return sum(filter(_is_number, elems))
