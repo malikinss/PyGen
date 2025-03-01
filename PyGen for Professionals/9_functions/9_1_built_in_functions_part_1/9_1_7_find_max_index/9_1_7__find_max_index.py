59b16c19e68bb6029461ede1ce330076b8752e38
Sam Malikin <workmalikin@gmail.com>
2025-03-01 17:20:14 +0200

    init 9_1_7

---
+++ PyGen for Professionals/9_functions/9_1_built_in_functions_part_1/9_1_7_find_max_index/9_1_7__find_max_index.py
@@ -0,0 +1,41 @@
+"""
+TODO:
+    You have access to the numbers list.
+
+    Complete the code below so that it prints the index of the maximum element
+    in this list.
+
+NOTE:
+    Indexing starts from zero.
+
+    It is convenient to use the enumerate() and max() functions in the task.
+"""
+from typing import List
+
+
+def find_max_index(numbers: List[int]) -> int:
+    """
+    Find the index of the maximum element in a list of integers.
+
+    Args:
+        numbers (List[int]): The list of integers.
+
+    Returns:
+        int: The index of the maximum element.
+    """
+    try:
+        # Find the maximum value in the list
+        max_value = max(numbers)
+
+        # Get the index of the maximum value
+        max_index = numbers.index(max_value)
+
+        return max_index
+    except ValueError:
+        print("The list is empty.")
+        return -1
+
+
+numbers = [-7724, 5023, 3197, -102, -4129, -880, 5857, -2866, -8913, 1195, 9809, 5347, -8071, 903, 3030, -4347, -3354, 1024, 8670, 4210, -5228, 8900, 4823, -2002, 4900, 9520, -3658, 1104, -9554, 3064, 9632, -8701, 3384, 4370, 2034, 7822, -9694, 3347, 7440, -8459, 3238, -5193, -3381, 5281, 9022, 5559, 7593, -6540, -6204, -2483, 8729, 5810, -8254, -9846, -1801, 4882, 3838, -3140, 7609, -3325, 6026, 2994, -1677, 1266, -1893, -4408, -5722, -2841, 9812, 5837, -7474, 4624, -664, 6998, 7888, -971, 8810, 3812, -5396, 2593, 512, -4634, 9735, -3062, 9031, -9300, 3657, 6332, 7552, 8125, -725, 4392, 1727, 8194, -2828, -4314, -8967, -7912, -1363, -5957]
+
+print(find_max_index(numbers))
