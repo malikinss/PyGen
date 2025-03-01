7fe296dd8aabd4c8900393750a015a44e2ea0dd9
Sam Malikin <workmalikin@gmail.com>
2025-03-01 16:09:43 +0200

    init 8_4_3

---
+++ PyGen for Professionals/8_recursion/8_4_recursion_part_4/8_4_3_get_value/8_4_3_get_value.py
@@ -0,0 +1,63 @@
+'''
+TODO:
+    Implement a get_value() function that takes two arguments in the following
+    order:
+        - nested_dicts is a dictionary containing arbitrary objects or
+        dictionaries as values, which in turn also contain arbitrary objects
+        or dictionaries as values; nesting can be arbitrary
+        - key is a hashable object
+
+    The function must determine the value that corresponds to the key key in
+    the nested_dicts dictionary or one of its nested dictionaries, and return
+    the result.
+
+NOTE:
+    It is guaranteed that the key key is present in the nested_dicts
+    dictionary or one of its nested dictionaries, and in a single copy.
+'''
+from typing import Dict, Hashable, Any
+
+
+def get_value(nested_dict: Dict[str, Any], key: Hashable) -> Any:
+    """
+    Retrieve the value associated with the given key from a nested dictionary.
+
+    Args:
+        nested_dict (dict): A dictionary that may contain other dictionaries.
+        key (Hashable): The key to search for in the nested dictionaries.
+
+    Returns:
+        Any: The value associated with the given key.
+    """
+    if key in nested_dict:
+        return nested_dict[key]
+
+    for sub_dict in nested_dict.values():
+        if isinstance(sub_dict, dict):
+            found_value = get_value(sub_dict, key)
+
+            # If found_value is not None, return it
+            if found_value is not None:
+                return found_value
+
+
+data = {
+    'firstName': 'Тимур',
+    'lastName': 'Гуев',
+    'birthDate': {
+        'day': 10,
+        'month': 'October',
+        'year': 1993
+    },
+    'address': {
+        'streetAddress': 'Часовая 25, кв. 127',
+        'city': {
+            'region': 'Московская область',
+            'type': 'город',
+            'cityName': 'Москва'
+        },
+        'postalCode': '125315'
+    }
+}
+
+print(get_value(data, 'cityName'))
