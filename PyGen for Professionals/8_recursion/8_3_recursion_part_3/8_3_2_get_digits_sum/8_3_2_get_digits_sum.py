aa9d8068d7de9244b679878553e4a091bcec9dc3
Sam Malikin <workmalikin@gmail.com>
2025-03-01 02:32:01 +0200

    init 8_3_2

---
+++ PyGen for Professionals/8_recursion/8_3_recursion_part_3/8_3_2_get_digits_sum/8_3_2_get_digits_sum.py
@@ -0,0 +1,26 @@
+'''
+TODO:
+    Write a program using recursion that takes a number as input and outputs
+    the sum of the digits of that number.
+'''
+
+
+def get_digits_sum(number: int) -> int:
+    """
+    Calculate the sum of the digits of a given number using recursion.
+
+    Args:
+        number (int): The number whose digits sum is to be calculated.
+
+    Returns:
+        int: The sum of the digits of the input number.
+    """
+    # Base case: if the number is a single digit, return it
+    if 0 <= number < 10:
+        return number
+
+    # Recursive case: add the last digit to the sum of the remaining digits
+    return number % 10 + get_digits_sum(number // 10)
+
+
+print(get_digits_sum(111211))
