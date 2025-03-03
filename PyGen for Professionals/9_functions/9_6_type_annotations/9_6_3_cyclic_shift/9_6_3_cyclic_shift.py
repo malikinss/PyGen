71e1300f1102eec010133e64aeb893cf99b46fd2
Sam Malikin <workmalikin@gmail.com>
2025-03-03 02:45:34 +0200

    init 9_6_3

---
+++ PyGen for Professionals/9_functions/9_6_type_annotations/9_6_3_cyclic_shift/9_6_3_cyclic_shift.py
@@ -0,0 +1,38 @@
+'''
+TODO:
+    Implement the cyclic_shift() function using type annotations that takes
+    two arguments in the following order:
+        numbers — a list of integers or floats
+        step — an integer
+
+    The function should modify the passed list, cyclically shifting the list
+    elements by step steps, and return None.
+
+    If step is a positive number, the shift is to the right;
+    if it is negative, the shift is to the left.
+
+NOTE:
+    Use the built-in types (list, tuple, ...), not the types from
+    the typing module.
+    Also use the | notation, not the Union type from the typing module.
+'''
+from collections import deque
+
+
+def cyclic_shift(numbers: list[int | float], step: int) -> None:
+    """
+    Cyclically shifts elements in the list by the specified number of steps.
+
+    Args:
+        numbers (list[int | float]): List of numbers to be shifted.
+        step (int): Number of steps to shift the list.
+        Positive values shift right, negative values shift left.
+
+    Returns:
+        None: The list is modified in place.
+    """
+    rotated_deque = deque(numbers)
+
+    rotated_deque.rotate(step)
+
+    numbers[:] = list(rotated_deque)
