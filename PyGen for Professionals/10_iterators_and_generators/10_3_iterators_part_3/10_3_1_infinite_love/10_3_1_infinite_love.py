0b641409f4f91303bb33cced23cc8c186b785dc2
Sam Malikin <workmalikin@gmail.com>
2025-03-05 20:23:01 +0200

    Create 10_3_1_infinite_love.py

---
+++ PyGen for Professionals/10_iterators_and_generators/10_3_iterators_part_3/10_3_1_infinite_love/10_3_1_infinite_love.py
@@ -0,0 +1,8 @@
+'''
+TODO:
+    Complete the code below so that the infinite_love variable
+    contains an iterator that infinitely generates a single
+    value, the string i love beegeek!.
+'''
+
+infinite_love = iter(lambda: 'i love beegeek!', '')
