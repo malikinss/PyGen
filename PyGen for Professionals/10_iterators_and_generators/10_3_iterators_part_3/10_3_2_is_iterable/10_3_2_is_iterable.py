6679f5bae9a1934e393aa27af04a794a39b2d52a
Sam Malikin <workmalikin@gmail.com>
2025-03-05 20:23:03 +0200

    Create 10_3_2_is_iterable.py

---
+++ PyGen for Professionals/10_iterators_and_generators/10_3_iterators_part_3/10_3_2_is_iterable/10_3_2_is_iterable.py
@@ -0,0 +1,48 @@
+'''
+TODO:
+    Implement a function is_iterable() that takes one argument:
+        obj — an arbitrary object
+
+    The function should return True if obj is an iterable object
+    or False otherwise.
+'''
+from typing import Iterable
+
+
+def is_iterable_1(obj: any) -> bool:
+    """
+    Check if the given object is iterable.
+
+    Args:
+        obj (Any): The object to check.
+
+    Returns:
+        bool: True if the object is iterable, otherwise False.
+    """
+    return '__iter__' in dir(obj)
+
+
+def is_iterable_2(obj: any) -> bool:
+    """
+    Check if the given object is iterable.
+
+    Args:
+        obj (Any): The object to check.
+
+    Returns:
+        bool: True if the object is iterable, otherwise False.
+    """
+    return isinstance(obj, Iterable)
+
+
+def is_iterable(obj: any) -> bool:
+    """
+    Check if the given object is iterable.
+
+    Args:
+        obj (Any): The object to check.
+
+    Returns:
+        bool: True if the object is iterable, otherwise False.
+    """
+    return hasattr(obj, '__iter__')
