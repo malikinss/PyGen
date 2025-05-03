de128ae19a7c5ec12cfbd3551d47f77e893c6a4d
Sam Malikin <workmalikin@gmail.com>
2025-05-03 22:43:16 +0300

    init 9_1_4_Progression

---
+++ PyGen OOP/9_class_design_tasks/9_1_tasks/9_1_4_Progression/9_1_4_Progression.py
@@ -0,0 +1,31 @@
+'''
+TODO:
+    Implement the ArithmeticProgression class to generate the terms of
+    the arithmetic progression.
+
+    When creating an instance of the ArithmeticProgression class, the first
+    term of the sequence and the difference of the progression must be
+    specified:
+        progression = ArithmeticProgression(0, 1)
+        for elem in progression:
+        if elem > 10:
+        break
+        print(elem, end=' ') # 0 1 2 3 4 5 6 7 8 9 10
+
+    Note that the arithmetic progression must be iterable and also infinite.
+
+    Similarly, implement the GeometricProgression class to generate the terms
+    of the geometric progression.
+
+    When creating an instance of the GeometricProgression class, the first
+    member of the sequence and the denominator of the progression must be
+    specified:
+        progression = GeometricProgression(1, 2)
+        for elem in progression:
+        if elem > 10:
+        break
+        print(elem, end=' ') # 1 2 4 8
+
+    A geometric progression, like an arithmetic one, must be iterable and also
+    infinite.
+'''
