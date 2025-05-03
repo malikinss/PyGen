208a5f1d8a80ccce1a24dfde8c7bb9ca4b394323
Sam Malikin <workmalikin@gmail.com>
2025-05-03 22:36:28 +0300

    init 9_1_1_anything

---
+++ PyGen OOP/9_class_design_tasks/9_1_tasks/9_1_1_anything/9_1_1_anything.py
@@ -0,0 +1,6 @@
+'''
+TODO:
+    Implement a function anything() that returns an object such that
+    the result of comparison with it using the operators ==, !=, >, <, >=,
+    and <= is always True.
+'''
