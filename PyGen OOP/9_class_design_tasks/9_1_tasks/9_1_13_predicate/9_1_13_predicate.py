d16a0c9885eb1f673fca6b5c40369e429c6a60b2
Sam Malikin <workmalikin@gmail.com>
2025-05-03 22:49:47 +0300

    init 9_1_13_predicate

---
+++ PyGen OOP/9_class_design_tasks/9_1_tasks/9_1_13_predicate/9_1_13_predicate.py
@@ -0,0 +1,56 @@
+'''
+TODO:
+    A predicate is a function that returns True or False depending on
+    the arguments passed.
+
+    Implement the @predicate decorator, which allows you to conveniently
+    combine predicates using the &, |, and ~ operators:
+        @predicate
+        def is_even(num):
+        return num % 2 == 0
+        @predicate
+        def is_positive(num):
+        return num > 0
+        print((is_even & is_positive)(4))
+        # True; equivalent to is_even(4) and is_positive(4)
+        print((is_even & is_positive)(3))
+        # False; equivalent to is_even(3) and is_positive(3)
+        print((is_even | is_positive)(3))
+        # True; equivalent to is_even(3) or is_positive(3)
+        print((~is_even & is_positive)(3))
+        # True; equivalent to not is_even(3) and is_positive(3)
+
+    The decorator should be able to work with any predicate, no matter how
+    many arguments it takes:
+        @predicate
+        def to_be():
+        return True
+        print((to_be | ~to_be)()) # True; equivalent to to_be() or not to_be()
+        @predicate
+        def is_equal(a, b):
+        return a == b
+        @predicate
+        def is_less_than(a, b):
+        return a < b
+        print((is_less_than | is_equal)(1, 2))
+        # True; equivalent to is_less_than(1, 2) or is_equal(1, 2)
+
+    Both positional and named arguments should also be supported:
+        print((is_less_than | is_equal)(2, b=2))
+        # True; equivalent to is_less_than(2, b=2) or is_equal(2, b=2)
+        print((is_less_than | is_equal)(a=3, b=2))
+        # False; equivalent to is_less_than(a=3, b=2) or is_equal(a=3, b=2)
+
+    The decorated function must be accessible outside combinations with other
+    functions and behave like the original function:
+        @predicate
+        def is_less_than(a, b):
+        return a < b
+        print(is_less_than(1, 2)) # True
+        print(is_less_than(2, 2)) # False
+        print(is_less_than(3, 2)) # False
+
+NOTE:
+    It is guaranteed that the functions being combined have the same
+    signatures.
+'''
